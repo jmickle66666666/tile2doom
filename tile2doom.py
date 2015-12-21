38f6147311e70a76eb9868c06207cb2ba444033b
jmickle <jaymickle@gmail.com>
2015-12-21 15:20:06 +0000

    <:>

---
+++ tile2doom.py
@@ -0,0 +1,66 @@
+import omg, random
+from PIL import Image
+
+owad = None
+omap = None
+
+def image_to_tiles(img):
+    pix = img.load()
+    output = [[-1 for y in range(img.size[0])] for x in range(img.size[1])]
+    for i in range(img.size[0]):
+        for j in range(img.size[1]):
+            if pix[i,j] == (0,0,0):
+                output[i][j] = 0
+    return output
+    
+def gentiles():
+    width = 30
+    height = 30
+    tiles = [[-1 for y in range(width)] for x in range(height)]
+    def digger(i,j):
+        length = 80
+        sid = random.choice([0,1,2,3])
+        while length > 0:
+            tiles[i][j] = 0
+            tiles[i+1][j] = 0
+            tiles[i][j+1] = 0
+            tiles[i+1][j+1] = 0
+            length -= 1
+            if sid == 0: i += 1
+            if sid == 1: j -= 1
+            if sid == 2: i -= 1
+            if sid == 3: j += 1
+            if random.choice([0,0,0,1]) == 1:
+                sid += random.choice([-1,1])
+                if sid == 4: sid = 0
+                if sid == -1: sid = 3
+        return (i,j)
+    last = digger(15,15)
+    return tiles
+
+def tile2doom(tiles):
+    player_placed = 0
+
+    temp_sidedef  = omg.mapedit.Sidedef(tx_mid="METAL2")
+
+    for i in range(len(tiles)):
+        for j in range(len(tiles[i])):
+            if tiles[i][j] == 0:
+                if player_placed == 0:
+                    player_placed = 1
+                    omap.things.append(omg.mapedit.Thing(x=(i*32)+16,y=(j*32)+16,type=0))
+                verts = []
+                verts.append((i * 32, j * 32))
+                verts.append(((i+1) * 32, j * 32))
+                verts.append(((i+1) * 32, (j+1) * 32))
+                verts.append((i * 32, (j+1) * 32))
+                omap.draw_sector(verts,sidedef=temp_sidedef)
+
+    owad.maps["MAP01"] = omap.to_lumps()
+
+if __name__=="__main__":
+    owad = omg.WAD()
+    omap = omg.mapedit.MapEditor()
+    #tile2doom(image_to_tiles(Image.open("test.png")))
+    tile2doom(gentiles())
+    owad.to_file("test.wad")
